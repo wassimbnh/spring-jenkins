4ea20a81f9d53505a8d011f816be8cae792875bf
Wassim <104159727+wassimbnh@users.noreply.github.com>
2023-11-12 19:42:21 +0100

    Update Stock.java

--- src/main/java/tn/esprit/devops_project/entities/Stock.java
+++ src/main/java/tn/esprit/devops_project/entities/Stock.java
@@ -6,6 +6,7 @@ import lombok.experimental.FieldDefaults;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Set;
+import javax.persistence.Table;
 
 @Entity
 @Getter
@@ -13,6 +14,7 @@ import java.util.Set;
 @AllArgsConstructor
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@Table(name = "Stock")
 public class Stock implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
